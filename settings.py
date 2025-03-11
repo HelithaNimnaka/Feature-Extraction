71de870970e73e0dfe2a74f167e4ea3fb97ffeb7
HelithaNimnaka <uzumakihelithanimnaka@gmail.com>
2025-03-11 09:33:14 +0530

    [add]

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""
+paths defined here are used in many places
+"""
+
+DATA_PATH = '/mnt/sda1/FYP_2024/Datasets/' # path for datasets
+EXPER_PATH = 'logs' # path for saving checkpoints
+SYN_TMPDIR = './datasets/'  # path for dumping synthetic data
+DEBUG = False # true: will make synthetic data only uses draw_checkboard and ignore other classes
+# DEBUG = False
+
